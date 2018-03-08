deebaa316ce7043c97006ed9e70dc3d0f4187d5c
Lucas Fischer <lucasfischerpt@gmail.com>
2018-03-08 12:07:40 +0000

    implemented getMatchedValues on Extractor.scala

--- reader/src/main/scala/Extractor.scala
+++ reader/src/main/scala/Extractor.scala
@@ -10,11 +10,19 @@ object Extractor {
 
   type Keyword = String
 
+  //A Map of keywords -> regular expressions that were tailored to obtain better results
   val knownRegEx: Map[Keyword, Regex] = Map(
     "name" -> new Regex("(?:name is)\\s+((?:[A-Z]\\w+\\s*){1,2})"),
-    "age" -> "(?:I'm|I am)\\s+(\\d{1,2})".r
+    "age" -> "(?:I'm|I am)\\s+(\\d{1,2})".r,
+    "mail" -> "((?:[a-z]|[0-9]|\\.|-|_)+@(?:[a-z]|\\.)+(?:pt|com|org|net|uk|co|eu))".r
   )
 
+  /**
+    * Method that given a file path (maybe change to a real file) will load that PDF file and read the text from it
+    *
+    * @param filePath - String containing the URI for the file to be loaded
+    * @return A String containing all the text found in the document
+    */
   def readPDF(filePath: String): String = {
     val pdf = PDDocument.load(new File(filePath))
     val document = new PDFTextStripper
@@ -25,11 +33,20 @@ object Extractor {
     str
   }
 
-  def getJSON(text: String, keywords: List[String], clientRegEx: Map[String, String] = null): List[String] = {
-    keywords.flatMap(key => {
-      if (knownRegEx.contains(key)) knownRegEx(key).findAllIn(text).matchData.map(_.group(1))
-      else
-        List("ups")
+  /**
+    * Method that will iterate through a list of given keywords and will try to obtain a value for that keyword
+    *
+    * @param text        - Text from the PDF extracted from readPDF method
+    * @param keywords    - List containing all the keywords we want to find values for
+    * @param clientRegEx - Optional parameter - If the client already has a predefined Regular Expression for a given key
+    *                    use that regular expression instead of ours
+    * @return a List containing pairs of Keywords and a list of values found for that keyword
+    */
+  def getMatchedValues(text: String, keywords: List[String], clientRegEx: Map[String, String] = null): List[(Keyword, List[String])] = {
+    keywords.map(key => {
+      if (knownRegEx.contains(key)) //if we already know a good RegEx for this keyword, use it
+        (key, knownRegEx(key).findAllIn(text).matchData.map(_.group(1)).toList)
+      else (key, List("ups")) //to be changed, here we need to manually search for the keywords in the text
     })
   }
 }
--- reader/src/main/scala/Main.scala
+++ reader/src/main/scala/Main.scala
@@ -6,6 +6,6 @@ object Main {
     val filepath: String = "/Users/Lucas Fischer/Documents/Flipper/reader/resources/test.pdf"
     val text = readPDF(filepath)
 
-    println(getJSON(text, List("name")))
+    println(getMatchedValues(text, List("name")))
   }
 }
