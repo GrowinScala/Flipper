09f2d3cef9777ef4b417cc0b7950fecf7790a9be
Margarida Reis <mreis@growin.pt>
2018-03-13 16:17:03 +0000

    created converter

--- converter/src/main/scala/Converter.scala
+++ converter/src/main/scala/Converter.scala
@@ -7,7 +7,12 @@ import org.apache.pdfbox.rendering.PDFRenderer
 object Converter {
 
 
-  def convertToImg(filePath: String): Unit = {
+  /**
+    * Method that converts a pdf file into a png image
+    *
+    * @param filePath - String containing the URI to the pdf file
+    */
+  def convertPDFtoPNG(filePath: String): Unit = {
         val pdf = PDDocument.load(new File(filePath))
         val catalog = pdf.getDocumentCatalog
         val renderer = new PDFRenderer(pdf)
