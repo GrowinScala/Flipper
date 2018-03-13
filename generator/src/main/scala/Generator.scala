2a846c7b04fcf0144b1c44b5e86a7344e2e57bb9
Margarida Reis <mreis@growin.pt>
2018-03-13 13:24:26 +0000

    implemented function that creates PDF with a given text (text can´t contain '\n')

--- generator/src/main/scala/Generator.scala
+++ generator/src/main/scala/Generator.scala
@@ -1,14 +1,27 @@
-import org.apache.pdfbox.pdmodel.{PDDocument, PDPage}
+import java.awt.Color
+
+import org.apache.pdfbox.pdmodel.font.PDType1Font
+import org.apache.pdfbox.pdmodel.{PDDocument, PDPage, PDPageContentStream}
 
 object Generator {
 
 
-  def createPDF():Unit={
+  def createPDF(text:String):Unit={
     val document = new PDDocument()
-    document.addPage(new PDPage())
+    val page = new PDPage()
+    document.addPage(page)
+
+    //val font=PDType1Font.HELVETICA
+    val contentStream = new PDPageContentStream(document,page)
+    contentStream.setFont(PDType1Font.HELVETICA_BOLD, 20)
+
+    contentStream.beginText()
+    contentStream.newLineAtOffset(100, 700)
+    contentStream.showText(text)
+    contentStream.endText()
+    contentStream.close()
 
     document.save("Blank_Document.pdf")
-
     document.close()
   }
 }
--- generator/src/main/scala/Main.scala
+++ generator/src/main/scala/Main.scala
@@ -3,6 +3,6 @@ object Main {
   import Generator._
 
   def main(args: Array[String]): Unit = {
-      createPDF()
+      createPDF("Hello World!!! My name is Margarida Reis.")
   }
 }
