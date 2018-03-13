071675b98d5d4d1e54c6f43528dd8d2c4252ebbd
Lucas Fischer <lucasfischerpt@gmail.com>
2018-03-13 11:32:55 +0000

    Added portuguese training file for OCR (optimal character recognition)

--- reader/src/main/scala/Main.scala
+++ reader/src/main/scala/Main.scala
@@ -3,7 +3,7 @@ object Main {
   import Extractor._
 
   def main(args: Array[String]): Unit = {
-    val filepath: String = "/Users/Lucas Fischer/Desktop/imgTester.pdf"
+    val filepath: String = "/Users/Lucas Fischer/Desktop/MegaTester.pdf"
     val text = readPDF(filepath)
 //    val objs = getAllObjects(text, List(("name", "NNP"), ("weight", "CD")))
 //    objs.foreach(o => println(makeJSONString(o)))
