049fd9c170c8eb47ef24e3537bedc2b6fd811e89
Margarida Reis <mreis@growin.pt>
2018-03-15 10:35:15 +0000

    Fixed dome of the formating of the pdf to odf file

--- converter/src/main/scala/Main.scala
+++ converter/src/main/scala/Main.scala
@@ -3,7 +3,7 @@ object Main {
   import Converter._
 
   def main(args: Array[String]): Unit = {
-    val filepath: String = "/Users/Margarida Reis/Desktop/test.pdf"
+    val filepath: String = "/Users/Margarida Reis/Desktop/MegaTester.pdf"
 //    convertPDFtoIMG(filepath,"jpeg")
     convertPDFtoODF(filepath)
   }
