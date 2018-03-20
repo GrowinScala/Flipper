b3d79e981034b4729a0ef5d111784b27146e403b
Lucas Fischer <lucasfischerpt@gmail.com>
2018-03-20 17:25:04 +0000

    Added POSTag Enum, working on implementing this enum in Extractor.scala methods

---
+++ reader/src/main/scala/POSTag.scala
@@ -0,0 +1,13 @@
+/**
+  * Enumeration for the standard POS tags which translate to both Portuguese and English
+  */
+object POSTag extends Enumeration {
+  val ADJ = Value("ADJ")
+  val PN = Value("PN")
+  val N = Value("N")
+  val NPLR = Value("NPLR")
+  val VB = Value("VB")
+  val VBN = Value("VBN")
+  val NUM = Value("NUM")
+  val ADV = Value("ADV")
+}
