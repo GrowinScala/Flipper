1e9553dc63a8d92b6f2b020877920fa169843ed9
Margarida Reis <mreis@growin.pt>
2018-03-26 09:30:37 +0100

    Minor fixes

--- reader/src/main/scala/Extractor.scala
+++ reader/src/main/scala/Extractor.scala
@@ -303,7 +303,9 @@ object Extractor {
     */
   private def cleanImageDir() {
     val dir = new File("./target/images")
-    val files = dir.listFiles.filter(_.isFile).toList
-    files.foreach(_.delete)
+    if(dir.exists) {
+      val files = dir.listFiles.filter(_.isFile).toList
+      files.foreach(_.delete)
+    }
   }
 }
