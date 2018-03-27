733abfb7fbf178fc8b1287451f154ac3e771ce51
Lucas Fischer <lucasfischerpt@gmail.com>
2018-03-27 10:13:22 +0100

    Code refactored to hide file handling and try-catch blocks

---
+++ generator/src/test/scala/FileHandlerSuite.scala
@@ -0,0 +1,9 @@
+import java.io.File
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class FileHandlerSuite extends FunSuite {
+
+}
