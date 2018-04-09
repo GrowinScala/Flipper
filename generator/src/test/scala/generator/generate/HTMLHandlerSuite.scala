f4aa9f9d11bff392de82ae75fb6e943efa915345
Lucas Fischer <lucasfischerpt@gmail.com>
2018-04-09 14:28:30 +0100

    Implementing HTMLHandlerSuite.scala

--- generator/src/test/scala/generator/generate/HTMLHandlerSuite.scala
+++ generator/src/test/scala/generator/generate/HTMLHandlerSuite.scala
@@ -9,6 +9,17 @@ import generator.utils._
 @RunWith(classOf[JUnitRunner])
 class HTMLHandlerSuite extends FunSuite {
 
+  val content =
+    Map(
+      "name" -> Content("name", "John Doe", H1(), "bigHeader"),
+    )
+
+  val configMap =
+    Map(
+      "bigHeader" -> Config("blue", "20", "center", "corbel", "bold")
+    )
+
+
   /**
     * Tests that passing a supported/corrected HTML entity will return the corresponding FormattingType eqiuvelant
     */
@@ -22,7 +33,23 @@ class HTMLHandlerSuite extends FunSuite {
         stringToHTMLTag("table") == Table() &&
         stringToHTMLTag("p") == P() &&
         stringToHTMLTag("span") == Text() &&
-        stringToHTMLTag("somethin-Else") == Text()
+        stringToHTMLTag("somethin-else") == Text()
     )
+  }
+
+  /**
+    * Tests that calling createCssString with the correct input parameters will result in the expected CSS String
+    */
+  test("correctly call createCssString") {
+    val config2 =
+      Map(
+        "bigHeader" -> Config("blue")
+      )
+
+    val expected = ".bigHeader{ color: blue; text-align: center; font-weight: bold; font-family: corbel; font-size: 20pt;} "
+    val expected2 = ".bigHeader{ color: blue; text-align: ; font-weight: ; font-family: ; font-size: pt;} "
+    val result = createCssString(content, configMap)
+    val result2 = createCssString(content, config2)
+    assert(result.equals(expected) && result2.equals(expected2))
   }
 }
