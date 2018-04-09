8347874046c6c63ecd7c56d0a454e3feb44fc2b8
Margarida Reis <mreis@growin.pt>
2018-04-09 12:02:23 +0100

    Refactor FileType to be case class

--- converter/src/main/scala/Main.scala
+++ converter/src/main/scala/Main.scala
@@ -1,13 +1,14 @@
 import java.io.File
 
-import parser.conversion.FileType
+import parser.conversion.{FileType, PNG}
 import parser.conversion.Converter._
 
 object Main {
 
   def main(args: Array[String]): Unit = {
     val filepath = new File("./converter/src/main/resources/cv.pdf")
-    convertPDFtoIMG(filepath, FileType.jpeg)
+    convertPDFtoIMG(filepath, PNG())
+//    println(PNG().toString)
 //    convertPDFtoODT(filepath)
   }
 }
