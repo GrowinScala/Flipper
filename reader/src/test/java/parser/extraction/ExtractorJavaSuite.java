09e9f20273b2288d3c57ff4657c31bd467e5bcd5
Margarida Reis <mreis@growin.pt>
2018-04-18 10:55:45 +0100

    reader's Java interface tests done

--- reader/src/test/java/parser/extraction/ExtractorJavaSuite.java
+++ reader/src/test/java/parser/extraction/ExtractorJavaSuite.java
@@ -27,7 +27,6 @@ public class ExtractorJavaSuite extends TestCase {
         keywords = new HashMap<>();
         keywords.put("name", SpecificationJava.postTag(POSTagEnum.PROPERNOUN));
         keywords.put("age", SpecificationJava.postTag(POSTagEnum.NUMBER));
-        keywords.put("phone", SpecificationJava.postTag(POSTagEnum.NUMBER, true));
 
         mapNull = null;
         mapEmpty = new HashMap<>();
@@ -39,6 +38,9 @@ public class ExtractorJavaSuite extends TestCase {
                    " These are just some examples.";
     }
 
+    /**
+     * Tests that calling readPDF with null returns NullPointerException
+     */
     public void testReadPDFNullFile(){
         try {
             ej.readPDF(null);
@@ -47,6 +49,9 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
+    /**
+     * Test that calling readPDF with invalid file returns NullPointerException
+     */
     public void testReadPDFInvalidFile() {
         try{
             ej.readPDF(new File(strEmpty));
@@ -55,6 +60,9 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
+    /**
+     * Tests if the result of calling getAllMatchedValues is correct or not
+     */
     public void testGetAllMatchedValues(){
         Map<String,List<String>> matchedValues = ej.getAllMatchedValues(strValid,keywords);
         String expValue = "Margarida Reis";
@@ -62,6 +70,9 @@ public class ExtractorJavaSuite extends TestCase {
         else assert (matchedValues.get("name").get(0).equals(expValue));
     }
 
+    /**
+     * Tests that passing a list of possible values to getAllMatchedValues will return one of the possibilities passed (assuming it exists in the text)
+     */
     public void testGetAllMatchedValuesOneOf(){
         ArrayList oneList = new ArrayList();
         oneList.add("blue");
@@ -73,6 +84,9 @@ public class ExtractorJavaSuite extends TestCase {
         assert(matchedValues.get("color").get(0).equals(expValue));
     }
 
+    /**
+     * Tests that passing a list of possible values to getAllMatchedValues will return multiple of the possibilities passed (assuming it exists in the text)
+     */
     public void testGetAllMatchedValuesMultiOf(){
         ArrayList multiList = new ArrayList();
         multiList.add("blue");
@@ -86,6 +100,9 @@ public class ExtractorJavaSuite extends TestCase {
         assert(matchedValues.get("color").equals(exptValue));
     }
 
+    /**
+     * Tests that passing a list of possible values that don't exist in the text will return an empty list in both OneOf and MultipleOf
+     */
     public void testGetAllMatchedValuesNoOneMulti(){
         ArrayList poss = new ArrayList();
         poss.add("does no exist");
@@ -98,21 +115,57 @@ public class ExtractorJavaSuite extends TestCase {
         assert (mVMul.get("color").isEmpty() && mVOne.get("color").isEmpty());
     }
 
+    /**
+     * Tests that passing a OneOf object with an empty possibilities list will result in an IllegalArgumentException
+     */
     public void testGetAllMatchedValuesOneEmpty(){
         ArrayList emptyPos = new ArrayList();
-
+        HashMap colorMap = new HashMap<>();
+        try{
+            colorMap.put("color",SpecificationJava.oneOf(emptyPos));
+            ej.getAllMatchedValues(strColor,colorMap);
+        } catch (Exception e) {
+            assert (e instanceof IllegalArgumentException);
+        }
     }
 
+    /**
+     * Tests that passing a MultipleOf object with an empty possibilities list will result in an IllegalArgumentException
+     */
+    public void testGetAllMatchedValuesMultiEmpty(){
+        ArrayList emptyPos = new ArrayList();
+        HashMap colorMap = new HashMap<>();
+        try{
+            colorMap.put("color",SpecificationJava.multipleOf(emptyPos));
+            ej.getAllMatchedValues(strColor,colorMap);
+        } catch (Exception e) {
+            assert (e instanceof IllegalArgumentException);
+        }
+    }
+
+    /**
+     * Tests that passing a null or empty text will return an empty list
+     */
     public void testGetAllMatchedValuesInvalidText(){
         Map nullRes = ej.getAllMatchedValues(strNull, keywords);
         Map emptyRes = ej.getAllMatchedValues(strEmpty,keywords);
         assert (nullRes.isEmpty() && emptyRes.isEmpty());
     }
 
+    /**
+     * Tests if the result of calling getAllMatchedValues with a keyword that does not exist in the text is empty
+     */
     public void testGetAllMatchedValuesInvalidKey(){
-
+        HashMap mapInv = new HashMap<>();
+        mapInv.put("color",SpecificationJava.postTag(POSTagEnum.NOUN));
+        Map<String,List<String>> matchedValues = ej.getAllMatchedValues(strValid,mapInv);
+        if(matchedValues.isEmpty()) fail("Matched value came back as an Empty List");
+        else assert (matchedValues.get("color").isEmpty());
     }
 
+    /**
+     * Tests that passing a null keywords map will result in a NullPointerException
+     */
     public void testGetAllMatchedValuesNullMap(){
         try {
             ej.getAllMatchedValues(strValid, mapNull);
@@ -121,6 +174,9 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
+    /**
+     * Tests that passing an empty keywords map will result in a IllegalArgumentException
+     */
     public void testGetAllMatchedValuesEmptyMap(){
         try {
             ej.getAllMatchedValues(strValid, mapEmpty);
@@ -129,12 +185,30 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
+    /**
+     * Tests if getSingleMatchedValue returns only one value for all keys
+     */
+    public void testGetSingleMatchedValue(){
+        Boolean assertRes = true;
+        Map<String,List<String>> matchedValue = ej.getSingleMatchedValue(strValid,keywords);
+        for (Map.Entry<String,List<String>> entry : matchedValue.entrySet()){
+            if(entry.getValue().size() != 1) assertRes = false;
+        }
+        assert (assertRes);
+    }
+
+    /**
+     * Tests that passing an empty or null text string to getSingleMatchedValue will return an empty list
+     */
     public void testGetSingleMatchedValueInvalidText(){
         Map nullRes = ej.getSingleMatchedValue(strNull,keywords);
         Map emptyRes = ej.getSingleMatchedValue(strEmpty,keywords);
         assert (nullRes.isEmpty() && emptyRes.isEmpty());
     }
 
+    /**
+     * Tests that passing a null keywords map to getSingleMatchedValue will result in a NullPointerException
+     */
     public void testGetSingleMatchedValueNullMap(){
         try {
             ej.getSingleMatchedValue(strValid, mapNull);
@@ -143,7 +217,10 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
-    public void testGetsingleMatchedValueEmptyMap(){
+    /**
+     * Tests that passing an empty keywords map to getSingleMatchedValue will result in a IlllegalArgumentException
+     */
+    public void testGetSingleMatchedValueEmptyMap(){
         try{
             ej.getSingleMatchedValue(strValid,mapEmpty);
         } catch (Exception e) {
@@ -151,12 +228,29 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
+    /**
+     * Tests if getSingleMatchedValue returns an empty value list if the given keyword does not exist in the text
+     */
+    public void testGetSingleMatchedValueInvalidKey(){
+        HashMap mapInv = new HashMap<>();
+        mapInv.put("color",SpecificationJava.postTag(POSTagEnum.NOUN));
+        Map<String,List<String>> matchedValue = ej.getSingleMatchedValue(strValid,mapInv);
+        if(matchedValue.isEmpty()) fail("Matched value came back as an Empty List");
+        else assert (matchedValue.get("color").isEmpty());
+    }
+
+    /**
+     * Tests that passing an empty or null text to getAllObjects will return an empty list
+     */
     public void testGetAllObjectsInvalidText(){
         Map nullRes = ej.getAllMatchedValues(strNull,keywords);
         Map emptyRes = ej.getAllMatchedValues(strEmpty,keywords);
         assert (nullRes.isEmpty() && emptyRes.isEmpty());
     }
 
+    /**
+     * Tests that passing a null map to getAllObjects will result in a NullPointerException
+     */
     public void testGetAllObjectsNullMap(){
         try{
             ej.getAllObjects(strValid,mapNull);
@@ -165,6 +259,9 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
+    /**
+     * Tests that passing an empty map to getAllObjects will result in a IllegalArgumentException
+     */
     public void testGetAllObjectsEmptyMap(){
         try{
             ej.getAllObjects(strValid,mapEmpty);
@@ -173,12 +270,50 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
+    /**
+     * Tests if getAllObjects returns a list with the correct information
+     */
+    public void testGetAllObjects(){
+        HashMap validKey = new HashMap<>();
+        validKey.put("name", SpecificationJava.postTag(POSTagEnum.PROPERNOUN));
+        List<Map<String,List<String>>> expected = new ArrayList<>();
+        List<String> auxList1 = new ArrayList<>();
+        auxList1.add("Margarida Reis");
+        List<String> auxList2 = new ArrayList<>();
+        auxList2.add("Lucas");
+        List<String> auxList3 = new ArrayList<>();
+        auxList3.add("Albertina");
+        List<String> auxList4 = new ArrayList<>();
+        auxList4.add("Justino Alberto");
+        HashMap auxMap1 = new HashMap<>();
+        auxMap1.put("name", auxList1);
+        HashMap auxMap2 = new HashMap<>();
+        auxMap2.put("name", auxList2);
+        HashMap auxMap3 = new HashMap<>();
+        auxMap3.put("name", auxList3);
+        HashMap auxMap4 = new HashMap<>();
+        auxMap4.put("name", auxList4);
+        expected.add(auxMap1);
+        expected.add(auxMap2);
+        expected.add(auxMap3);
+        expected.add(auxMap4);
+        List res = ej.getAllObjects(strValid,validKey);
+        if(res.isEmpty()) fail("getAllObjects returned an enpty List");
+        else assert (res.equals(expected));
+    }
+
+    /**
+     * Tests that calling getJsonObjects with null or empty String returns empty Lists
+     */
     public void testGetJsonObjectsInvalidText(){
         List nullRes = ej.getJSONObjects(strNull,keywords);
         List emptyRes = ej.getJSONObjects(strEmpty,keywords);
         assert (nullRes.isEmpty() && emptyRes.isEmpty());
     }
 
+    /**
+     * Tests that calling getJsonObjects with null map returns NullPointerException
+     */
     public void testGetJsonObjectsNullMap(){
         try{
             ej.getJSONObjects(strValid,mapNull);
@@ -187,6 +322,9 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
+    /**
+     * Tests that calling getJasonObjects with empty map returns IllegalArgumentsException
+     */
     public void testGetJsonObjectsEmptyMap(){
         try{
             ej.getJSONObjects(strValid,mapEmpty);
@@ -195,12 +333,31 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
+    /**
+     * Tests if the matched values with the given keywords generates the correct JSON strings for all the expected objects
+     */
+    public void testGetAllJsonObjects(){
+        List expected = new ArrayList<>();
+        expected.add("{\"name\" : \"Margarida Reis\", \"age\" : 25}");
+        expected.add("{\"name\" : \"Lucas\", \"age\" : 21}");
+        expected.add("{\"name\" : \"Albertina\", \"age\" : \"\"}");
+        expected.add("{\"name\" : \"Justino Alberto\", \"age\" : \"\"}");
+        List<String> objRes = ej.getJSONObjects(strValid,keywords);
+        assert (objRes.equals(expected));
+    }
+
+    /**
+     * Tests that calling getSingleJson with null or empty string returns "{}"
+     */
     public void testGetSingleJsonInvalidText(){
         Boolean nullRes = ej.getSingleJSON(strNull,keywords).equals(emptyJson);
         Boolean emptyRes = ej.getSingleJSON(strEmpty,keywords).equals(emptyJson);
         assert (nullRes && emptyRes);
     }
 
+    /**
+     * Tests that calling getsingleJson with null map returns NullPointerException
+     */
     public void testGetSingleJsonNullMap(){
         try{
             ej.getSingleJSON(strValid,mapNull);
@@ -209,6 +366,9 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
+    /**
+     * Tests that calling makeJsonString with empty map returns IllegalArgumentException
+     */
     public void testGetSingleJsonEmptyMap(){
         try{
             ej.getSingleJSON(strValid,mapEmpty);
@@ -217,6 +377,9 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
+    /**
+     * Tests that calling makeJsonString with null map returns NullPointerException
+     */
     public void testMakeJsonStringNullMap(){
         try {
             ej.makeJSONString(mapNull);
@@ -225,14 +388,17 @@ public class ExtractorJavaSuite extends TestCase {
         }
     }
 
+    /**
+     * Tests that making a JSON String with an empty map will return an empty JSON object ("{}")
+     */
     public void testMakeJsonStringEmptyMap(){
-        try{
-            ej.makeJSONString(mapEmpty);
-        } catch (Exception e){
-            assert (e instanceof IllegalArgumentException);
-        }
+        String res = ej.makeJSONString(mapEmpty);
+        assert (res.equals(emptyJson));
     }
 
+    /**
+     * Tests that calling getJsonFromForm with empty or null String returns "{}"
+     */
     public void testGetJsonFromForm(){
         Boolean emptyRes = ej.getJSONFromForm(strEmpty).equals(emptyJson);
         Boolean nullRes = ej.getJSONFromForm(strNull).equals(emptyJson);
